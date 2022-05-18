563016df58a9d8cbddc91db6e8962fcce466f636
Davi Albino <negroalbino_@hotmail.com>
2022-05-18 10:54:35 -0300

    Adicionando reuisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,8 +30,7 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+  
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
     // Teste se o retorno da função é um array.
@@ -39,5 +38,31 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
+
+  it('Verifica se productDetails é uma função.', () => {
+    expect(typeof productDetails).toBe('function');
   });
+
+  it('Verifica se o retorno da função é um array.', () => {
+    expect(typeof productDetails().length).toBe('number');
+  })
+
+  it('Verifica se o array retornado pela função contém dois itens dentro.', () => {
+    expect(productDetails().length).toBe(2);
+  })
+
+  it('Verifica se os dois itens dentro do array retornado pela função são objetos.', () => {
+    expect(typeof productDetails()[0]).toBe('object');
+    expect(typeof productDetails()[1]).toBe('object');
+  })
+
+  it('Verifica se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    expect(productDetails('Detergente', 'Sabonete')[0] !== productDetails('Detergente', 'Sabonete')[1]).toBeTruthy();
+  })
+
+  it('Verifica se os dois productIds terminam com 123.', () => {
+    expect(productDetails('Detergente', 'Sabonete')[0].details.productId).toBe(`Detergente123`);
+    expect(productDetails('Detergente', 'Sabonete')[1].details.productId).toBe(`Sabonete123`);
+  })
+
 });
